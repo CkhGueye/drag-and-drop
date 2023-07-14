82b64e131300c6465b475b1565a96540b7f669bb
CkhGueye <alabaygueye@gmail.com>
2023-07-14 12:18:10 +0100

    footer component

---
+++ src/components/footer/Index.jsx
@@ -0,0 +1,10 @@
+import "./footer.scss";
+
+export default function Footer() {
+  return (
+    <div className="footer">
+      Designed and coded by{" "}
+      <a href="https://linkedin.com/in/cheikhouwgueye">cheikhouw</a>
+    </div>
+  );
+}
